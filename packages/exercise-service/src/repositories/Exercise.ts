35eba34bcaf682e4ca40d3049b79a5311dfd969d
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2023-08-31 14:44:46 +0200

    fix: getAllJoinUser

--- packages/exercise-service/src/repositories/Exercise.ts
+++ packages/exercise-service/src/repositories/Exercise.ts
@@ -6,11 +6,12 @@ import { ExerciseToCreate as IApiExerciseToCreate } from '../../types/api/Exerci
 import IExerciseJoinUser from '../../types/ExerciseJoinUser'
 
 export const create = async (data: IApiExerciseToCreate): Promise<boolean> => {
-  const sql = `INSERT INTO exercise(id, user_id, content, created_at) VALUES("${crypto.randomUUID()}", "${data.user_id}", "${data.content}", "${new Date()}")`
+  const sql = `INSERT INTO exercise(id, user_id, content, created_at) VALUES(?,?,?,?)`
 
   return new Promise((resolve, reject) => {
     db.run(
       sql,
+      [crypto.randomUUID(), data.user_id, data.content, new Date()],
       (e) => {
         if (e) {
           console.error(e)
@@ -39,7 +40,7 @@ export const getAll = async (): Promise<IRepositoryExercise[]> => {
 }
 
 export const getAllJoinUser = async (): Promise<IExerciseJoinUser[]> => {
-  const sql = 'SELECT * FROM exercise JOIN user ON exercise.user_id = user.id'
+  const sql = 'SELECT exercise.id, exercise.user_id, exercise.content, exercise.created_at, user.name FROM exercise JOIN user ON exercise.user_id = user.id'
 
   return new Promise((resolve, reject) => {
     db.all(sql, [], (e, rows: IExerciseJoinUser[]) => {
